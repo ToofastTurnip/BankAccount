3fd6ded0e90535121902943e2a0d31bff6a5c0fb
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-07-19 18:08:30 -0400

    Transfer changes

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,18 +1,18 @@
 DROP TABLE util_account_types;
-DROP TABLE util_exchange_types;
+DROP TABLE util_transfer_types;
 DROP TABLE account;
 DROP TABLE users;
-DROP TABLE exchange;
+DROP TABLE transfer;
 
 CREATE TABLE util_account_types (
 account_type_id NUMBER (10, 0) NOT NULL AUTO_INCREMENT,
 name VARCHAR2(255) NOT NULL,
 PRIMARY KEY (account_type_id));
 
-CREATE TABLE util_exchange_types (
-exchange_type_id NUMBER(10, 0) NOT NULL AUTO_INCREMENT,
+CREATE TABLE util_transfer_types (
+transfer_type_id NUMBER(10, 0) NOT NULL AUTO_INCREMENT,
 name VARCHAR2(255) NOT NULL,
-PRIMARY KEY (exchange_type_id));
+PRIMARY KEY (transfer_type_id));
 
 CREATE TABLE users (
 user_id NUMBER(10, 0) NOT NULL AUTO_INCREMENT,
@@ -34,14 +34,14 @@ PRIMARY KEY (account_type_id),
 FOREIGN KEY (user_id) REFERENCES users (user_id),
 FOREIGN KEY (account_type_id) REFERENCES util_account_types (account_type_id));
 
-CREATE TABLE exchange (
-exchange_id NUMBER(10,0) NOT NULL AUTO_INCREMENT,
-exchange_type_id NUMBER(10, 0) NOT NULL,
+CREATE TABLE transfer (
+transfer_id NUMBER(10,0) NOT NULL AUTO_INCREMENT,
+transfer_type_id NUMBER(10, 0) NOT NULL,
 account_id NUMBER(10,0) NOT NULL,
 amount FLOAT(10, 2) NOT NULL,
 exchange_date DATE NOT NULL,
-PRIMARY KEY (exchange_id),
-FOREIGN KEY (exchange_type_id) REFERENCES util_exchange_types (exchange_type_id),
+PRIMARY KEY (transfer_id),
+FOREIGN KEY (transfer_type_id) REFERENCES util_transfer_types (transfer_type_id),
 FOREIGN KEY (account_id) REFERENCES account (account_id));
 
 DROP SEQUENCE hibernate_sequence;
@@ -51,10 +51,10 @@ CREATE SEQUENCE hibernate_sequence;
 INSERT INTO util_account_types VALUES (1, "Checking");
 INSERT INTO util_account_types VALUES (2, "Savings");
 
-INSERT INTO util_exchange_types VALUES (1, "Withdrawl");
-INSERT INTO util_exchange_types VALUES (2, "Deposit");
+INSERT INTO util_transfer_types VALUES (1, "Withdraw");
+INSERT INTO util_transfer_types VALUES (2, "Deposit");
 
-// Passwords hashed with BCrypt
+-- Passwords hashed with BCrypt
 INSERT INTO users VALUES (1, "daenis", "$2a$04$v5ylBEn3qptg6l5SVq7lquy.RAs3jhznTRpvkzIZbg09MKql3LobG", "Dennis", "Kalaygian", "example@gmail.com"); // password: daenis31
 INSERT INTO users VALUES (2, "toofast", "$2a$04$nRhrbDhae9LghZpLQkkZyeM1krLUs1LaR9Q3MEkOtBAhjMLyEB/am", "Mitch", "Taylor", "example@verizon.com"); // password: toofast31
 
